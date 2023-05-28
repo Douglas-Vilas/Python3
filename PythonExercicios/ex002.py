5884610ba4522e531950b8c0a6e34f58399b551d
Douglas Vilas <douglas_vilass@hotmail.com>
2023-05-28 13:30:12 -0300

    Exercício 2
    Usando o .format(nomeVariavel), para uma melhor organização e visibilidade em nosso código.

---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,3 @@
+nome = input('Digite o seu nome: ')
+# Nesse exercício, estamos usando a .format(nomeVariavel), para formatar melhor o print.
+print('Prazer em te conhecer, {}! Seja bem Vindo!'.format(nome))
