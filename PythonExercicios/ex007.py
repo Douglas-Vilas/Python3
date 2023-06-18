40206706c348995429262c0f8432886fb49f2fec
Douglas Vilas <douglas_vilass@hotmail.com>
2023-06-18 17:30:12 -0300

    Exercício 007
    Média Aritmética.

---
+++ PythonExercicios/ex007.py
@@ -0,0 +1,12 @@
+# Esse exercício solicita ler as duas notas de um aluno, e calcular e motrar a média.
+
+# Notas do Aluno
+nota1 = float(input('Digite uma nota: '))
+nota2 = float(input('Digite outra nota: '))
+
+# Soma das notas e média
+media = (nota1 + nota2) / 2
+
+# Exibir o resultado para o usuário
+print('O total da nota é: {}.'.format(nota1 + nota2))
+print('A média da nota é: {}'.format(media))
